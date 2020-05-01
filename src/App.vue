f53a2afee4ad53467693df52eb6096cf29686fd6
ine <inec@users.noreply.github.com>
2020-04-30 20:49:34 -0500

    broken

--- src/App.vue
+++ src/App.vue
@@ -8,22 +8,17 @@
     </div>
   </div>
 </template>
+
 <script>
-import HeaderBar from "@/components/header-bar";
-import Heroes from "@/components/heroes";
+import HeaderBar from "./components/header-bar";
+import Heroes from "./components/heroes";
 
 export default {
   name: "App",
   components: { HeaderBar, Heroes }
 };
 </script>
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+<style lang="scss">
+@import "@/design/index.scss";
 </style>
